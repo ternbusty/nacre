b151c313720c26ac4b06f8fa945345da8d5d025f
ternbusty <a.iwasaki0128@gmail.com>
2026-09-17 13:50:54 +0900

    fix: align readonlyPaths, maskedPaths and checkBeforeUpdate with runc
    Three fixes for runc-bats compatibility:
    
    1. readonlyPaths: remove MS_REC from remount and preserve existing mount
       flags via statfs (runc does statfs then remounts without MS_REC)
    
    2. _init_post_pivot ordering: set_rootfs_readonly -> readonlyPaths ->
       maskedPaths to match runc's finalizeRootfs -> readonlyPath -> maskPath
    
    3. _check_memory_before_update: collect all validation errors before
       calling fatal, so both memory and memory+swap messages are reported

--- lib/Nacre/Mount.pm
+++ lib/Nacre/Mount.pm
@@ -11,7 +11,7 @@ use Socket qw(AF_UNIX SOCK_STREAM);
 use Cwd qw(abs_path);
 use Nacre::Const qw(
     SYS_mount SYS_umount2 SYS_pivot_root SYS_mknod SYS_mknodat
-    SYS_open_tree SYS_move_mount SYS_mount_setattr
+    SYS_open_tree SYS_move_mount SYS_mount_setattr SYS_statfs
     CLONE_NEWUSER
     MS_RDONLY MS_NOSUID MS_NODEV MS_NOEXEC MS_REMOUNT
     MS_NOATIME MS_NODIRATIME MS_BIND MS_MOVE MS_REC MS_SILENT
@@ -1110,9 +1110,19 @@ sub apply_readonly_paths ($paths) {
     for my $p (@{$paths // []}) {
         next unless -e $p;
         do_mount($p, $p, '', MS_BIND | MS_REC, '');
-        do_mount('', $p, '', MS_REMOUNT | MS_BIND | MS_RDONLY | MS_REC, '');
+        my $flags = _statfs_flags($p);
+        do_mount('', $p, '', $flags | MS_REMOUNT | MS_BIND | MS_RDONLY, '');
     }
     return;
+}
+
+sub _statfs_flags ($path) {
+    my $buf = "\0" x 120;
+    my $nr = SYS_statfs + 0;
+    my $ret = syscall($nr, $path, $buf);
+    return 0 if $ret != 0;
+    my $f_flags = unpack('q', substr($buf, 80, 8));
+    return $f_flags & (MS_NOSUID | MS_NODEV | MS_NOEXEC);
 }
 
 sub set_rootfs_readonly ($rootfs_readonly) {
